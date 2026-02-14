255d92b6e20cb98d920d1e04cc135e680fa1c729
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-02-14 15:31:58 +0000

    task4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    all_names = dir(hidden_4)
-    for name in sorted(all_names):
+    names = dir(hidden_4)
+    for name in sorted(names):
         if not name.startswith("__"):
             print("{}".format(name))
