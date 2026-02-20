0d89faa8f8180cd6bd893a283fb3ab2b12916c55
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-02-20 10:36:43 +0000

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,14 +3,13 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
+    rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     for i in range(len(roman_string)):
-        val = roman_dict.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and val < roman_dict.get(roman_string[i + 1], 0):
+        val = rd.get(roman_string[i], 0)
+        next_v = rd.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) \
+            else 0
+        if val < next_v:
             res -= val
         else:
             res += val
