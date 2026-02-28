f2ba532820672e4036542edcf34b40ba8ffa9d98
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-02-28 09:15:25 +0000

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Class Square that defines a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
