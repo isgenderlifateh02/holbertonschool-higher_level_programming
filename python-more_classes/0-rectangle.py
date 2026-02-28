c2ef93fb38b4e1280b21f8395eef61ff72814f13
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-02-28 10:13:12 +0000

    task0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,63 +1,9 @@
 #!/usr/bin/python3
 """
-This module defines a Rectangle class.
-It includes private instance attributes width and height.
+This module defines an empty Rectangle class.
 """
 
 
 class Rectangle:
-    """Class that represents a rectangle."""
-
-    def __init__(self, width=0, height=0):
-        """Initializes a new Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Retrieves the width of the rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Sets the width of the rectangle with validation.
-
-        Args:
-            value (int): The value to set the width to.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Retrieves the height of the rectangle."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Sets the height of the rectangle with validation.
-
-        Args:
-            value (int): The value to set the height to.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    """An empty Rectangle class."""
+    pass
