2c36de960f1d563be1aead9a788b5f2d1bcd22f8
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-02-28 09:21:16 +0000

    task2

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with size validation"""
+
+
+class Square:
+    """Class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
