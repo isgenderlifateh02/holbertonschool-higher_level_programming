c6a7b402f1240496c7a752435df111ffcd381e3d
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-07 11:27:59 +0000

    task0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,11 +6,5 @@ and methods of an object.
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
-    
-    Args:
-        obj: The object to inspect.
-        
-    Returns:
-        A list of strings representing the attributes and methods.
     """
     return dir(obj)
