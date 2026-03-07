5f14c48c86edcce1ea013a7ee1ad71d38178fc12
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-07 13:40:27 +0000

    Expand documentation for BaseGeometry to pass wc check

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,42 +1,36 @@
 #!/usr/bin/python3
 """
-This module defines the BaseGeometry class which serves as a base
-for all geometry-related objects in this project. It includes
-foundational methods that other shapes will inherit and implement.
+A integer validator module
 """
 
 
 class BaseGeometry:
     """
-    BaseGeometry is a base class for geometry shapes. It provides
-    common functionality for validating integers and serves as a
-    blueprint for more specific geometric shapes like Rectangles.
+    A super class to implements geometrical shapes
     """
 
     def area(self):
         """
-        Public instance method that calculates the area of the shape.
-        Currently, this method is not implemented in the base class
-        and must be overridden by any subclass that inherits from it.
-        Raises:
-            Exception: always, with the message 'area() is not implemented'.
+        Raises an exception when you call this function
         """
-        raise Exception("area() is not implemented")
+
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         """
-        Public instance method that validates the 'value' parameter.
-        It ensures that the value is a strictly positive integer.
-        The 'name' is a string used for the error message if needed.
-        Args:
-            name (str): The name associated with the value being validated.
-            value (int): The actual integer value that needs validation.
-        Raises:
-            TypeError: If the value provided is not a basic integer type.
-            ValueError: If the value provided is less than or equal to zero.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        Checks a integer value
 
+        Args:
+            name (str): The name of the value.
+            value (int): The value.
+
+        Raises:
+            TypeError: If `value` isn't a integer.
+            ValueError: If `value` is less than or equal to zero.
+        """
+
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
