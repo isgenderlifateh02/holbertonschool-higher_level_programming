ba53901c28f6770b58f19c55065bd94edaafd196
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-14 15:18:25 +0000

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""
-Bu modul mətn fayllarını oxumaq üçün funksiya təqdim edir.
-"""
 
 
 def read_file(filename=""):
-    """
-    UTF8 formatında olan mətn faylını oxuyur və çap edir.
-    
-    Args:
-        filename (str): Oxunacaq faylın adı.
-    """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
