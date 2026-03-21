e0491dd735066373339aad3464de294b77968c89
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-21 13:36:33 +0000

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,3 @@
--- Lists all cities in hbtn_0d_usa, joined with their state
-SELECT cities.name, states.name 
-FROM cities 
-INNER JOIN states ON cities.state_id = states.id 
-ORDER BY cities.id ASC;
+-- Lists cities in the database
+-- Output: cities.id - cities.name - states.name
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
