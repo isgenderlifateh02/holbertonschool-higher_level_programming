82da26fe0abc8f5130567e4427414bb1df38d905
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-21 20:13:09 +0000

    task 13: clean delete

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,23 +10,15 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Engine yaradılır
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    # Session yaradılır
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Adında 'a' hərfi olan bütün ştatları tapırıq
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    session.query(State).filter(State.name.like('%a%')).delete(
+        synchronize_session=False
+    )
 
-    # Hər birini session-dan silirik
-    for state in states_to_delete:
-        session.delete(state)
-
-    # Dəyişiklikləri bazaya tətbiq edirik
     session.commit()
-
-    # Session bağlanır
     session.close()
