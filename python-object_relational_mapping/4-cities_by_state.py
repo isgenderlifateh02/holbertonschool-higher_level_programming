798c6de052e7ec842eed5a758f25ab8392d15ade
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-21 19:56:11 +0000

    task 4 fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
 """
 Lists all cities from the database hbtn_0e_4_usa.
-Results are sorted in ascending order by cities.id.
+Sorted by cities.id in ascending order.
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db_user = sys.argv[1]
-    db_password = sys.argv[2]
-    db_name = sys.argv[3]
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=db_user,
-        passwd=db_password,
-        db=db_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
     cursor = db.cursor()
-    
-    # JOIN istifadə edərək şəhərləri və onlara uyğun ştat adlarını çəkirik
-    query = "SELECT cities.id, cities.name, states.name \
-             FROM cities \
-             INNER JOIN states ON cities.state_id = states.id \
-             ORDER BY cities.id ASC"
-    
-    cursor.execute(query)
-    query_rows = cursor.fetchall()
-
-    for row in query_rows:
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                   "FROM cities "
+                   "INNER JOIN states ON cities.state_id = states.id "
+                   "ORDER BY cities.id ASC")
+    for row in cursor.fetchall():
         print(row)
-
     cursor.close()
     db.close()
