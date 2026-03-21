a82332cc9b460568cfd8bdb55a206daed6cdb9ce
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-21 19:58:05 +0000

    task 5 clean fix

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -15,14 +15,12 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities \
-                   INNER JOIN states ON cities.state_id = states.id \
-                   WHERE states.name = %s \
-                   ORDER BY cities.id ASC", (sys.argv[4],))
-    
-    rows = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities "
+                "INNER JOIN states ON cities.state_id = states.id "
+                "WHERE states.name = %s "
+                "ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
-    
-    cursor.close()
+    cur.close()
     db.close()
