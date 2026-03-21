687fa8167c1dbf670978960f925880020bf72fa0
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-03-21 13:29:18 +0000

    task 8

--- SQL_more_queries/8-cities_of_california_details.sql
+++ SQL_more_queries/8-cities_of_california_details.sql
@@ -1,3 +1,3 @@
--- Lists all cities of California that can be found in the database hbtn_0d_usa
--- The states table contains only one record where name = California
+-- Lists all cities of California in the database hbtn_0d_usa
+-- Results are sorted by id in ascending direction
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
